5244bf1233319bc7a5b25574185217c18b3601c2
sbxlmdsl <sbxlm@qq.com>
2023-11-15 10:15:31 +0800

    Update github.install.bat

--- github.install.bat
+++ github.install.bat
@@ -3,10 +3,10 @@ setlocal
 git submodule init
 git submodule update plum
 
-set rime_version=9.5.16
+set rime_version=9.5.17
 
-set download_archive=rime-47c4ab3-Windows.7z
-set download_archive_deps=rime-deps-47c4ab3-Windows.7z
+set download_archive=rime-2e5e293-Windows-msvc.7z
+set download_archive_deps=rime-deps-2e5e293-Windows-msvc.7z
 
 curl -LO https://github.com/sbxlmdsl/librime/releases/download/%rime_version%/%download_archive%
 curl -LO https://github.com/sbxlmdsl/librime/releases/download/%rime_version%/%download_archive_deps%
@@ -20,4 +20,3 @@ copy /Y librime\dist\lib\rime.dll output\
 
 if not exist output\data\opencc mkdir output\data\opencc
 copy /Y librime\share\opencc\*.* output\data\opencc\
-
